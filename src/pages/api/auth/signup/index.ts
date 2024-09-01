e15003f7d2b373dc25e403da7159056276a00400
João Vinny <119421047+vinnytherobot@users.noreply.github.com>
2024-09-01 16:41:48 -0300

    Update index.ts

--- src/pages/api/auth/signup/index.ts
+++ src/pages/api/auth/signup/index.ts
@@ -2,34 +2,37 @@ import { PrismaClient } from "@prisma/client"
 import { NextApiRequest, NextApiResponse } from "next"
 
 import z from "zod"
-
 import bcrypt from "bcryptjs"
 
 const prisma = new PrismaClient();
 
-export async function POST(request: NextApiRequest, response: NextApiResponse){
-    const createUserSchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string()
-    })
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === "POST") {
+        const createUserSchema = z.object({
+            name: z.string(),
+            email: z.string().email(),
+            password: z.string()
+        });
 
-    const { name, email, password } = createUserSchema.parse(request.body);
+        try {
+            const { name, email, password } = createUserSchema.parse(req.body);
 
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+            const hashedPassword = await bcrypt.hash(password, 10);
+            
+            const newUser = await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                    password: hashedPassword
+                }
+            });
 
-        const newUser = await prisma.user.create({
-            data: {
-                name,
-                email,
-                password: hashedPassword
-            }
-        })
-
-        response.status(201).json({ message: "User created successfully", id: newUser.id });
-
-    } catch(error){
-        response.status(400).json({ error: "Failed to create user" });
+            res.status(201).json({ message: "User created successfully", id: newUser.id });
+        } catch (error) {
+            res.status(400).json({ error: "Failed to create user" });
+        }
+    } else {
+        res.setHeader("Allow", ["POST"]);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
