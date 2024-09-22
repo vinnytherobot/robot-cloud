cb897fa8d2c2ed528621c0bdd0b27198ecf70ae6
vinnytherobot <dacostamacielf@gmail.com>
2024-09-22 09:54:19 -0300

    Google Search Console added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="b47D27KqHRaaYv1hRdZMLZWHtojim5txYVNvso0Xdkc" />
+      </Head>
       <body>
         <Main />
         <NextScript />
